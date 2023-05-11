6d14fc3a6e026fc7149c74f9169c721ca29bf589
ermal <ermal@cloudstock.io>
2023-05-11 14:26:43 +0200

    Added some Twiggy

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,18 +2,31 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 use function Symfony\Component\String\u;
 
 
-class VinylController
+class VinylController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('Title: PB and jams');
+        $tracks = [
+            ['song' => 'MORNING LIGHT', 'artist' => 'JUSTIN TIMBERLAKE & ALICIA KEYS'],
+            ['song' =>'BONNIE & CLYDE ', 'artist' => ' JAY-Z & BEYONCE'],
+            ['song' =>'SCARTISSUE ', 'artist' => ' RED HOT CHILI PEPPERS'],
+            ['song' =>'LOVE IS THE NEW MONEY ', 'artist' => ' ANDY GRAMMER'],
+            ['song' =>'E DUA ', 'artist' => ' VEDAT ADEMI X KLEJTI MAHMUTAJ'],
+            ['song' => 'Habibi', 'artist' => 'Anxhela Peristeri']
+        ];
+
+        return $this->render('vinyl/homepage.html.twig', [
+            'title' => 'PB and jams',
+            'tracks' => $tracks,
+        ]);
     }
 
     #[Route('/browse/{slug}')]
