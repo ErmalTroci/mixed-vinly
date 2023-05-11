0e784784daf5798ebbef401d2069ed1b7f8deabb
ermal <ermal@cloudstock.io>
2023-05-12 00:13:02 +0200

    Added styles and an api json endpoint

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -11,7 +11,7 @@ use function Symfony\Component\String\u;
 
 class VinylController extends AbstractController
 {
-    #[Route('/')]
+    #[Route('/', name: 'app_homepage')]
     public function homepage(): Response
     {
         $tracks = [
@@ -29,10 +29,12 @@ class VinylController extends AbstractController
         ]);
     }
 
-    #[Route('/browse/{slug}')]
+    #[Route('/browse/{slug}', name: 'app_browse')]
     public function browse(string $slug = null): Response
     {
-        $title = $slug ? 'Genre: '.u(str_replace('-', ' ', $slug))->title(true) : 'All Geres';
-        return new Response($title);
+        $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : 'All Geres';
+        return $this->render('vinyl/browse.html.twig', [
+            'genre' => $genre
+        ]);
     }
 }
